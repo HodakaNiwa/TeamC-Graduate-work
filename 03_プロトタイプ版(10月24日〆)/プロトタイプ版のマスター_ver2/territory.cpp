2ef06a57c1030f9b3e53c7cda0d347de2ea995c0
丹羽保貴 <kousuke.0117@outlook.com>
2019-10-24 15:25:05 +0900

    プロトタイプ版のマスター_ver2追加

---
+++ 03_プロトタイプ版(10月24日〆)/プロトタイプ版のマスター_ver2/territory.cpp
@@ -0,0 +1,503 @@
+//=============================================================================
+//
+// テリトリー処理 [territory.cpp]
+// Author : 佐藤安純 Sato_Asumi
+//
+//=============================================================================
+#define _CRT_SECURE_NO_WARNINGS
+#include "Territory.h"
+#include "manager.h"
+#include "renderer.h"
+#include "input.h"
+#include "collision.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "library.h"
+#include "game.h"
+#include "Player.h"
+#include "loadEffect.h"
+
+//=============================================================================
+// マクロ定義
+//=============================================================================
+#define SaveTextName ("data/TEXT/SaveTerritory.txt")		//保存するテキスト名
+#define UPDATE_LENGTH	(80.0f)								//更新状態を有効にする範囲
+#define GET_LENGTH		(40.0f)								//取得範囲		
+#define MIN_TERRITORY	(3)									//取得テリトリーの最低数
+
+//=============================================================================
+// 前方宣言
+//=============================================================================
+LPDIRECT3DTEXTURE9 * CTerritory::m_ppTexture = NULL;
+LPD3DXMESH * CTerritory::m_pMesh = NULL;
+LPD3DXBUFFER * CTerritory::m_pBuffMat = NULL;
+DWORD *	CTerritory::m_pNumMat = NULL;
+D3DXVECTOR3 * CTerritory::m_pVtxMax = NULL;
+D3DXVECTOR3 * CTerritory::m_pVtxMin = NULL;
+int CTerritory::m_nMaxTex = 0;
+int CTerritory::m_nMaxModel = 0;
+int CTerritory::m_nCountObj = 0;
+int CTerritory::m_nMaxObj = 0;
+D3DXCOLOR CTerritory::m_DefaultColor = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);
+
+//=============================================================================
+// 生成処理
+//=============================================================================
+CTerritory * CTerritory::Create(const D3DXVECTOR3 pos, D3DXVECTOR3 rot, D3DXVECTOR3 scale, int nTexNum, int ModelNum, int nPoint)
+{
+	//インスタンスの生成
+	CTerritory * pTerritory = new CTerritory;
+
+	//初期化処理
+	pTerritory->Init();
+	pTerritory->SetPos(pos);						//位置の設定
+	pTerritory->SetRot(rot);						//向きの設定
+	pTerritory->SetvtxMax(m_pVtxMax[ModelNum]);		//最大値
+	pTerritory->SetvtxMin(m_pVtxMin[ModelNum]);		//最小値
+	pTerritory->SetScale(scale);					//大きさの設定
+	pTerritory->SetTexNum(nTexNum);					//テクスチャ番号
+	pTerritory->SetModelType(ModelNum);				//モデル番号
+	pTerritory->SetPoint(nPoint);					//ポイントの保存
+	pTerritory->CreateCollider();
+
+	//モデル・テクスチャの割り当て
+	pTerritory->BindModel(m_pMesh[ModelNum], m_pBuffMat[ModelNum], m_pNumMat[ModelNum], m_ppTexture[nTexNum]);
+
+	return pTerritory;
+}
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CTerritory::CTerritory() : CSceneX(3, OBJTYPE_TERRITORY)
+{
+	m_pos = D3DXVECTOR3(0, 0, 0);
+	m_rot = D3DXVECTOR3(0, 0, 0);
+}
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CTerritory::CTerritory(int nPriority, OBJTYPE objtype) : CSceneX(nPriority, objtype)
+{
+	m_pos = D3DXVECTOR3(0, 0, 0);
+	m_rot = D3DXVECTOR3(0, 0, 0);
+	m_Scale = D3DXVECTOR3(0, 0, 0);
+}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CTerritory::~CTerritory(){}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT CTerritory::Init(void)
+{
+	//変数の初期化
+	m_nTex = 0;
+	m_nModelType = 0;
+	m_nPoint = 1;
+	m_Color = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);	//色
+	m_OldColor = D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f);	//色
+	m_nNumPlayer = -1;								//プレイヤー番号
+	m_nOldNumPlayer = -1;							//前回のプレイヤー番号
+	m_fLength = 0.0f;								//長さ
+	m_bGetTerritory = false;						//テリトリーを取得したかどうか
+	
+	if (m_pLoadEffect == NULL)
+	{
+		m_pLoadEffect = CLoadEffect::Create(0, m_pos, 8);
+		//m_pLoadEffect->Init();
+	}
+
+	CSceneX::Init();
+
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CTerritory::Uninit(void)
+{
+	CSceneX::Uninit();
+
+	if (m_pLoadEffect != NULL)
+	{
+		m_pLoadEffect->Uninit();
+		delete m_pLoadEffect;
+		m_pLoadEffect = NULL;
+	}
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CTerritory::Update(void)
+{
+	CSceneX::Update();
+
+	for (int nCntPlayer = 0; nCntPlayer < MAX_NUMPLAYER; nCntPlayer++)
+	{
+		CPlayer * pPlayer = CManager::GetGame()->GetPlayer(nCntPlayer);
+		D3DXVECTOR3 PlayerPos = pPlayer->GetPos();
+
+		//プレイヤーとの距離を求める
+		m_fLength = CFunctionLib::Vec2Length(m_pos.x, m_pos.z, PlayerPos.x, PlayerPos.z);
+
+		//更新範囲にいるかどうか
+		if (m_fLength < GET_LENGTH)
+		{
+			pPlayer->AddTerritoryList(this);	//プレイヤーに自身の情報を渡す
+		}
+	}
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CTerritory::CreateGetEffect(void)
+{
+	if (m_pLoadEffect != NULL)
+	{
+		m_pLoadEffect->SetPos(D3DXVECTOR3(m_pos.x, 50.0f, m_pos.z));
+		m_pLoadEffect->OnTimeEffect();
+	}
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CTerritory::Draw(void)
+{
+	SetDiffuseColor(m_Color);
+	CSceneX::Draw();
+	SetDiffuseColor(m_DefaultColor);
+
+	if (m_pLoadEffect != NULL)
+	{
+		m_pLoadEffect->Draw();
+	}
+}
+
+//=============================================================================
+//　取得状態の更新処理
+//=============================================================================
+void CTerritory::SetDiffuseColor(D3DXCOLOR Color)
+{
+	//マテリアル情報の取得
+	LPD3DXBUFFER ModelMat = CSceneX::GetBuffMat();
+	DWORD nNumMat = CSceneX::GetNumMat();
+
+	//マテリアルポインタの取得
+	D3DXMATERIAL * pMat = (D3DXMATERIAL*)ModelMat->GetBufferPointer();
+
+	//マテリアルカラーの変更
+	for (int nCntMat = 0; nCntMat < (int)nNumMat; nCntMat++)
+	{
+		pMat[nCntMat].MatD3D.Diffuse = Color;
+	}
+}
+
+//=============================================================================
+//　プレイヤー番号の保存
+//=============================================================================
+void CTerritory::SetPlayerNumber(int nNumPlayer)
+{
+	m_nOldNumPlayer = m_nNumPlayer;	//前回の情報を保存
+	m_nNumPlayer = nNumPlayer;
+}
+
+//=============================================================================
+// 当たり判定の生成
+//=============================================================================
+void CTerritory::CreateCollider(void)
+{
+	// コライダーの生成
+	CColliderManager *pColManager = CColliderManager::Create(1);
+	if (pColManager == NULL) { return; }
+	CBoxCollider *pBox = CBoxCollider::Create(m_pos, D3DXVECTOR3(1.0f, 1.0f, 1.0f), 27.0f, 68.0f, 20.0f, true);
+	pBox->SetParent(this);
+	//pBox->SetParentMtxWorld(&GetMtx());
+	pColManager->SetCollider(pBox, 0);
+	SetCOlliderManager(pColManager);
+}
+
+//=============================================================================
+// テクスチャの読み込み
+//=============================================================================
+void CTerritory::LoadTex(CLoadTextTerritory::TEX_INFO TexInfo)
+{
+	//デバイスの取得
+	LPDIRECT3DDEVICE9 pDevice = CManager::GetRenderer()->GetDevice();
+
+	//テクスチャの最大数
+	m_nMaxTex = TexInfo.nMaxTex;
+
+	//テクスチャメモリを動的確保
+	if (m_ppTexture == NULL)
+	{
+		m_ppTexture = new LPDIRECT3DTEXTURE9[m_nMaxTex];
+	}
+
+	//テクスチャを生成する
+	for (int nCntTex = 0; nCntTex < m_nMaxTex; nCntTex++)
+	{
+		// テクスチャの生成
+		D3DXCreateTextureFromFile(pDevice, &TexInfo.ppFileName[nCntTex][0], &m_ppTexture[nCntTex]);
+	}
+}
+
+//=============================================================================
+// テクスチャの破棄
+//=============================================================================
+void CTerritory::UnloadTex(void)
+{
+	if (m_ppTexture != NULL)
+	{
+		for (int nCntTex = 0; nCntTex < m_nMaxTex; nCntTex++)
+		{
+			m_ppTexture[nCntTex]->Release();
+			m_ppTexture[nCntTex] = NULL;
+		}
+
+		delete[] m_ppTexture;
+		m_ppTexture = NULL;
+	}
+}
+
+//=============================================================================
+//　モデルの読み込み
+//=============================================================================
+void CTerritory::LoadModel(CLoadTextTerritory::OBJ_INFO ObjInfo)
+{
+	//デバイスの取得
+	LPDIRECT3DDEVICE9 pDevice = CManager::GetRenderer()->GetDevice();
+
+	//モデルの最大数
+	m_nMaxModel = ObjInfo.nMaxModel;
+
+	//モデルの動的確保
+	if ((m_pMesh == NULL) && (m_pBuffMat == NULL) && (m_pNumMat == NULL))
+	{
+		m_pMesh = new LPD3DXMESH[m_nMaxModel];
+		m_pBuffMat = new LPD3DXBUFFER[m_nMaxModel];
+		m_pNumMat = new DWORD[m_nMaxModel];
+		m_pVtxMax = new D3DXVECTOR3[m_nMaxModel];
+		m_pVtxMin = new D3DXVECTOR3[m_nMaxModel];
+	}
+
+	//モデルの生成
+	for (int nCntModel = 0; nCntModel < m_nMaxModel; nCntModel++)
+	{
+		// Xファイルの読み込み
+		D3DXLoadMeshFromX(&ObjInfo.ppModelFileName[nCntModel][0],
+			D3DXMESH_SYSTEMMEM,
+			pDevice,
+			NULL,
+			&m_pBuffMat[nCntModel],
+			NULL,
+			&m_pNumMat[nCntModel],
+			&m_pMesh[nCntModel]);
+
+		//変数宣言
+		int		nNumVtx = 0;	//頂点数
+		DWORD	sizeFVF = 0;	//頂点フォーマットのサイズ
+		BYTE *	pVtxBuff = 0;	//頂点バッファへのポインタ
+
+								//頂点数を取得
+		nNumVtx = m_pMesh[nCntModel]->GetNumVertices();
+
+		//頂点フォーマットのサイズを取得
+		sizeFVF = D3DXGetFVFVertexSize(m_pMesh[nCntModel]->GetFVF());
+
+		m_pVtxMax[nCntModel] = D3DXVECTOR3(-100000.0f, 0.0f, -100000.0f);	//頂点の最大値
+		m_pVtxMin[nCntModel] = D3DXVECTOR3(100000.0f, 0.0f, 100000.0f);		//頂点の最小値
+
+																			//頂点バッファをロック
+		m_pMesh[nCntModel]->LockVertexBuffer(D3DLOCK_READONLY, (void**)&pVtxBuff);
+
+		for (int nCntVtx = 0; nCntVtx < nNumVtx; nCntVtx++)
+		{
+			D3DXVECTOR3 vtx = *(D3DXVECTOR3*)pVtxBuff;	//頂点座標の代入
+
+														//最大値を比較
+			if (m_pVtxMax[nCntModel].x < vtx.x)
+			{
+				m_pVtxMax[nCntModel].x = vtx.x;
+			}
+			if (m_pVtxMax[nCntModel].y < vtx.y)
+			{
+				m_pVtxMax[nCntModel].y = vtx.y;
+			}
+			if (m_pVtxMax[nCntModel].z < vtx.z)
+			{
+				m_pVtxMax[nCntModel].z = vtx.z;
+			}
+
+			//最小値を比較
+			if (m_pVtxMin[nCntModel].x > vtx.x)
+			{
+				m_pVtxMin[nCntModel].x = vtx.x;
+			}
+			if (m_pVtxMin[nCntModel].y > vtx.y)
+			{
+				m_pVtxMin[nCntModel].y = vtx.y;
+			}
+			if (m_pVtxMin[nCntModel].z > vtx.z)
+			{
+				m_pVtxMin[nCntModel].z = vtx.z;
+			}
+			//サイズ分ポインタを進める
+			pVtxBuff += sizeFVF;
+		}
+
+		//頂点バッファをアンロック
+		m_pMesh[nCntModel]->UnlockVertexBuffer();
+	}
+}
+
+//=============================================================================
+// モデルの破棄
+//=============================================================================
+void CTerritory::UnloadModel(void)
+{
+	//メッシュの破棄
+	if (m_pMesh != NULL)
+	{
+		for (int nCntModel = 0; nCntModel < m_nMaxModel; nCntModel++)
+		{
+			m_pMesh[nCntModel]->Release();
+			m_pMesh[nCntModel] = NULL;
+		}
+
+		delete[] m_pMesh;
+		m_pMesh = NULL;
+	}
+
+	//バッファの破棄
+	if (m_pBuffMat != NULL)
+	{
+		for (int nCntModel = 0; nCntModel < m_nMaxModel; nCntModel++)
+		{
+			m_pBuffMat[nCntModel]->Release();
+			m_pBuffMat[nCntModel] = NULL;
+		}
+
+		delete[] m_pBuffMat;
+		m_pBuffMat = NULL;
+	}
+
+	//マテリアルの破棄
+	if (m_pNumMat != NULL)
+	{
+		delete m_pNumMat;
+		m_pNumMat = NULL;
+	}
+
+	//最大値の破棄
+	if (m_pVtxMax != NULL)
+	{
+		delete m_pVtxMax;
+		m_pVtxMax = NULL;
+	}
+
+	//最小値の破棄
+	if (m_pVtxMin != NULL)
+	{
+		delete m_pVtxMin;
+		m_pVtxMin = NULL;
+	}
+}
+
+//=============================================================================
+//　オブジェクト検索
+//=============================================================================
+CTerritory * CTerritory::RequestPointer(int nCountObj)
+{
+	//敵のオブジェクトポインタの取得
+	CScene * pSceneTop = CScene::GetTop(TERRITORY_PRIORITY);	 //オブジェクトポインタの先頭を取得			
+	CScene * pScene = pSceneTop;						 //トップの位置を保存する
+	CTerritory * m_pTerritory = NULL;
+	int nNumObject = 0;
+
+	while (pScene != NULL)
+	{
+		CScene * pSceneNext = pScene->GetpNext();					//次のオブジェクトのポインタを保存する
+
+		if (CScene::OBJTYPE_TERRITORY == pScene->GetObjType())
+		{
+			if (nNumObject == nCountObj)
+			{//番号が一致したらポインタを保存
+				m_pTerritory = (CTerritory*)pScene;
+				break;
+			}
+
+			nNumObject++;	//番号の加算
+		}
+
+		pScene = pSceneNext;		//ポインタを進める
+	}
+
+	return m_pTerritory;
+}
+
+//=============================================================================
+// テキスト保存処理
+//=============================================================================
+void CTerritory::Save(HWND hWnd)
+{
+	//変数宣言
+	FILE * pFile = NULL;
+
+	//テクストの作成
+	pFile = fopen(SaveTextName, "w");
+
+	//書き出し処理
+	if (pFile != NULL)
+	{
+		//敵のオブジェクトポインタの取得
+		CScene * pSceneTop = CScene::GetTop(TERRITORY_PRIORITY);	 //オブジェクトポインタの先頭を取得			
+		CScene * pScene = pSceneTop;						 //トップの位置を保存する
+
+		while (pScene != NULL)
+		{
+			CScene * pSceneNext = pScene->GetpNext();					//次のオブジェクトのポインタを保存する
+
+			if (CScene::OBJTYPE_TERRITORY == pScene->GetObjType())
+			{
+				if (pScene->GetDeth() == false)
+				{
+					CTerritory * pObject = (CTerritory*)pScene;						//敵クラスにキャストする
+					int ModelType = pObject->GetModelType();
+					int nTexNum = pObject->GetTex();
+					D3DXVECTOR3 Pos = pObject->GetPos();
+					D3DXVECTOR3 Rot = pObject->GetRot();
+					D3DXVECTOR3 Scale = pObject->GetScale();
+					int nPoint = pObject->GetPoint();
+
+					fprintf(pFile, "SET\n");
+					fprintf(pFile, "\tMODELTYPE = %d\n", ModelType);							//モデルのタイプ
+					fprintf(pFile, "\tTEX = %d\n", nTexNum);									//テクスチャ番号
+					fprintf(pFile, "\tPOS = %.1f %.1f %.1f\n", Pos.x, Pos.y, Pos.z);			//位置
+					fprintf(pFile, "\tROT = %.1f %.1f %.1f\n", Rot.x, Rot.y, Rot.z);			//向き
+					fprintf(pFile, "\tSCALE = %.1f %.1f %.1f\n", Scale.x, Scale.y, Scale.z);	//スケール
+					fprintf(pFile, "\tPOINT = %d\n", nPoint);									//ポイント
+					fprintf(pFile, "END_SET\n");
+					fprintf(pFile, "\n");
+				}
+			}
+
+			pScene = pSceneNext;		//ポインタを進める
+		}
+
+		//書き込みを終了する
+		fclose(pFile);
+
+		//メッセージボックスの表示
+		MessageBox(hWnd, "セーブが完了しました", "処理完了通知", MB_OK);
+	}
+}
---
+++ 03_プロトタイプ版(10月24日〆)/プロトタイプ版のマスター_ver2/title.cpp
@@ -0,0 +1,409 @@
+//=============================================================================
+//
+// タイトルの処理 [title.cpp]
+// Author : 佐藤安純
+//
+//=============================================================================
+#include "title.h"
+#include "main.h"
+#include "renderer.h"
+#include "manager.h"
+#include "input.h"
+#include "fade.h"
+#include "sound.h"
+#include "ranking.h"
+#include "loadText.h"
+#include "library.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define FADE_TIME	(60 * 10)
+#define LOAD_UI		("data\\TEXT\\UI\\Title.txt")
+
+
+// タイトルの歯車用
+#define TITLE_GEAR_MAX        (5)
+#define TITLE_GEAR_TEXIDX     (3)
+#define TITLE_GEAR_PRIORITY   (5)
+
+#define TITLE_GEAR_POS0       (D3DXVECTOR3(300.0f, 360.0f, 0.0f))
+#define TITLE_GEAR_POS1       (D3DXVECTOR3(365.0f, 140.0f, 0.0f))
+#define TITLE_GEAR_POS2       (D3DXVECTOR3(SCREEN_WIDTH / 2.0f, 260.0f, 0.0f))
+#define TITLE_GEAR_POS3       (D3DXVECTOR3(920.0f, 360.0f, 0.0f))
+#define TITLE_GEAR_POS4       (D3DXVECTOR3(985.0f, 160.0f, 0.0f))
+
+#define TITLE_GEAR_SIZE0      (D3DXVECTOR3(125.0f, 125.0f, 0.0f))
+#define TITLE_GEAR_SIZE1      (D3DXVECTOR3(150.0f, 150.0f, 0.0f))
+#define TITLE_GEAR_SIZE2      (D3DXVECTOR3(200.0f, 200.0f, 0.0f))
+#define TITLE_GEAR_SIZE3      (D3DXVECTOR3(150.0f, 150.0f, 0.0f))
+#define TITLE_GEAR_SIZE4      (D3DXVECTOR3(125.0f, 125.0f, 0.0f))
+
+#define TITLE_GEAR_ROTSPEED0  (-D3DX_PI * 0.004f)
+#define TITLE_GEAR_ROTSPEED1  (D3DX_PI * 0.002f)
+#define TITLE_GEAR_ROTSPEED2  (D3DX_PI * 0.001f)
+#define TITLE_GEAR_ROTSPEED3  (D3DX_PI * 0.002f)
+#define TITLE_GEAR_ROTSPEED4  (-D3DX_PI * 0.004f)
+
+#define TITLE_GEAR_TEXHEIGHT0 (-1.0f)
+#define TITLE_GEAR_TEXHEIGHT1 (1.0f)
+#define TITLE_GEAR_TEXHEIGHT2 (1.0f)
+#define TITLE_GEAR_TEXHEIGHT3 (1.0f)
+#define TITLE_GEAR_TEXHEIGHT4 (-1.0f)
+
+//*****************************************************************************
+// プロトタイプ宣言
+//*****************************************************************************
+
+//*****************************************************************************
+// 静的メンバ変数
+//*****************************************************************************
+
+//=============================================================================
+// デフォルトコンストラクタ
+//=============================================================================
+CTitle::CTitle(){}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CTitle::~CTitle(){}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+void CTitle::Init(void)
+{
+	//ベースの初期化
+	CModeBase::Init();
+
+	//UI情報の読み込み処理
+	LoadUI(LOAD_UI);
+
+	// タイトル用の歯車を生成
+	CreateTitleGear();
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CTitle::Uninit(void)
+{
+	//ベースの破棄
+	CModeBase::Uninit();
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CTitle::Update(void)
+{
+	//サウンドの情報
+	CSound *pSound = CManager::GetSound();
+
+	//	タイトルのカウンター加算
+	m_nCounter++;
+
+	//インプットの取得　
+	CGamePad	*pGamePad = CManager::GetInputGamePad();
+	CInputKeyboard * pKeyboard = CManager::GetInputkeyboard();
+
+	if (pKeyboard->GetKeyboardTrigger(DIK_RETURN) == true)
+	{
+		if (CFade::GetFadeMode() != CFade::FADE_OUT)
+		{
+			CFade::SetFade(CManager::MODE_SELECT);
+		}
+	}
+	if ((m_nCounter > FADE_TIME))
+	{
+		if (CFade::GetFadeMode() != CFade::FADE_OUT)
+		{
+			CFade::SetFade(CManager::MODE_RANKING);
+		}
+	}
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CTitle::Draw(void)
+{
+	CScene::DrawAll();
+}
+
+//=============================================================================
+// タイトル用の歯車を生成する処理
+//=============================================================================
+void CTitle::CreateTitleGear(void)
+{
+	D3DXVECTOR3 pos[TITLE_GEAR_MAX] =
+	{
+		TITLE_GEAR_POS0,
+		TITLE_GEAR_POS1,
+		TITLE_GEAR_POS2,
+		TITLE_GEAR_POS3,
+		TITLE_GEAR_POS4,
+	};
+	D3DXVECTOR3 Size[TITLE_GEAR_MAX] =
+	{
+		TITLE_GEAR_SIZE0,
+		TITLE_GEAR_SIZE1,
+		TITLE_GEAR_SIZE2,
+		TITLE_GEAR_SIZE3,
+		TITLE_GEAR_SIZE4,
+	};
+	float fRotSpeed[TITLE_GEAR_MAX] =
+	{
+		TITLE_GEAR_ROTSPEED0,
+		TITLE_GEAR_ROTSPEED1,
+		TITLE_GEAR_ROTSPEED2,
+		TITLE_GEAR_ROTSPEED3,
+		TITLE_GEAR_ROTSPEED4,
+	};
+	float fTexHeight[TITLE_GEAR_MAX] =
+	{
+		TITLE_GEAR_TEXHEIGHT0,
+		TITLE_GEAR_TEXHEIGHT1,
+		TITLE_GEAR_TEXHEIGHT2,
+		TITLE_GEAR_TEXHEIGHT3,
+		TITLE_GEAR_TEXHEIGHT4,
+	};
+
+	for (int nCntGear = 0; nCntGear < TITLE_GEAR_MAX; nCntGear++)
+	{
+		CTitleGear2D *pGear2D = CTitleGear2D::Create(pos[nCntGear], Size[nCntGear], fRotSpeed[nCntGear], fTexHeight[nCntGear], TITLE_GEAR_PRIORITY);
+		if (pGear2D == NULL) { continue; }
+		pGear2D->BindTexture(GetTexture()[TITLE_GEAR_TEXIDX]);
+	}
+}
+
+
+
+//*****************************************************************************
+// タイトルロゴ用の歯車クラスの処理(CTitleGear2D)
+//*****************************************************************************
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CTitleGear2D::CTitleGear2D(int nPriority, OBJTYPE objType) : CScene2D(nPriority, objType)
+{
+	m_fRot = 0.0f;
+	m_fRotSpeed = 0.0f;
+	m_fAngle = 0.0f;
+	m_fLength = 0.0f;
+}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CTitleGear2D::~CTitleGear2D()
+{
+
+}
+
+//=============================================================================
+// 生成処理
+//=============================================================================
+CTitleGear2D *CTitleGear2D::Create(D3DXVECTOR3 pos, D3DXVECTOR3 Size, float fRotSpeed, float fTexHeight, int nPriority, CScene2D::TYPE type)
+{
+	// メモリの確保
+	CTitleGear2D *pTitleGear2D = NULL;
+	pTitleGear2D = new CTitleGear2D(nPriority);
+	if (pTitleGear2D == NULL) { return NULL; }
+
+	// 各種値の設定
+	pTitleGear2D->SetRotSpeed(fRotSpeed);
+
+	// 初期化処理
+	if (FAILED(pTitleGear2D->Init()))
+	{
+		pTitleGear2D->Uninit();
+		pTitleGear2D = NULL;
+		return NULL;
+	}
+
+	// 大きさの設定
+	pTitleGear2D->Set(pos, Size);
+
+	// テクスチャ座標の設定
+	pTitleGear2D->SetTexUV(0.0f, 1.0f, 0.0f, fTexHeight);
+
+	// ポリゴンの大きさを計算しておく
+	pTitleGear2D->CalcPolygonScale();
+
+	return pTitleGear2D;
+}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT CTitleGear2D::Init(void)
+{
+	// 共通の初期化処理
+	if (FAILED(CScene2D::Init()))
+	{
+		return E_FAIL;
+	}
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CTitleGear2D::Uninit(void)
+{
+	CScene2D::Uninit();
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CTitleGear2D::Update(void)
+{
+	// 向きを回転させる
+	m_fRot += m_fRotSpeed;
+	RotationPolygon();
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CTitleGear2D::Draw(void)
+{
+	// レンダリングクラスの取得
+	CRenderer *pRenderer = CManager::GetRenderer();
+	if (pRenderer == NULL) { return; }
+
+	// デバイスの取得
+	LPDIRECT3DDEVICE9 pDevice = pRenderer->GetDevice();
+	if (pDevice == NULL) { return; }
+
+	// 加算合成する設定に
+	pDevice->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_ADD);
+	pDevice->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	pDevice->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_ONE);
+
+	// 共通の描画処理
+	CScene2D::Draw();
+
+	// αブレンディングを元に戻す
+	pDevice->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_ADD);
+	pDevice->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
+	pDevice->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+}
+
+//=============================================================================
+// ポリゴンの大きさを計算する処理
+//=============================================================================
+void CTitleGear2D::CalcPolygonScale(void)
+{
+	// 大きさを取得
+	D3DXVECTOR3 Size = GetSize();
+
+	// ポリゴンの対角線の角度求める
+	m_fAngle = (float)atan2(Size.x, Size.y);
+
+	// 対角線の長さを求める
+	m_fLength = sqrtf((Size.x * Size.x) + (Size.y * Size.y));
+}
+
+//=============================================================================
+// ポリゴンを回転させる処理
+//=============================================================================
+void CTitleGear2D::RotationPolygon(void)
+{
+	// 頂点情報
+	VERTEX_2D *pVtx;
+
+	// 頂点バッファをロックし,頂点データへのポインタを取得
+	LPDIRECT3DVERTEXBUFFER9 pVtxBuff = GetVtxBuff();
+	if (pVtxBuff == NULL) { return; }
+	pVtxBuff->Lock(0, 0, (void**)&pVtx, 0);
+
+	// 頂点の回転を考慮して座標を計算
+	float XPos[4];
+	float YPos[4];
+	XPos[0] = CFunctionLib::RotationVectorX(m_fRot, -sinf(m_fAngle) * m_fLength, -cosf(m_fAngle) * m_fLength);
+	YPos[0] = CFunctionLib::RotationVectorY(m_fRot, -sinf(m_fAngle) * m_fLength, -cosf(m_fAngle) * m_fLength);
+	XPos[1] = CFunctionLib::RotationVectorX(m_fRot, sinf(m_fAngle) * m_fLength, -cosf(m_fAngle) * m_fLength);
+	YPos[1] = CFunctionLib::RotationVectorY(m_fRot, sinf(m_fAngle) * m_fLength, -cosf(m_fAngle) * m_fLength);
+	XPos[2] = CFunctionLib::RotationVectorX(m_fRot, -sinf(m_fAngle) * m_fLength, cosf(m_fAngle) * m_fLength);
+	YPos[2] = CFunctionLib::RotationVectorY(m_fRot, -sinf(m_fAngle) * m_fLength, cosf(m_fAngle) * m_fLength);
+	XPos[3] = CFunctionLib::RotationVectorX(m_fRot, sinf(m_fAngle) * m_fLength, cosf(m_fAngle) * m_fLength);
+	YPos[3] = CFunctionLib::RotationVectorY(m_fRot, sinf(m_fAngle) * m_fLength, cosf(m_fAngle) * m_fLength);
+
+	// 頂点座標
+	for (int nCntVer = 0; nCntVer < 4; nCntVer++)
+	{// 頂点の数だけ繰り返し
+		pVtx[nCntVer].pos = D3DXVECTOR3(XPos[nCntVer], YPos[nCntVer], 0.0f) + GetPos();
+	}
+
+	// 頂点バッファをアンロックする
+	pVtxBuff->Unlock();
+}
+
+//=============================================================================
+// 現在の向きを設定する処理
+//=============================================================================
+void CTitleGear2D::SetRot(const float fRot)
+{
+	m_fRot = fRot;
+}
+
+//=============================================================================
+// 回転スピードを設定する処理
+//=============================================================================
+void CTitleGear2D::SetRotSpeed(const float fRotSpeed)
+{
+	m_fRotSpeed = fRotSpeed;
+}
+
+//=============================================================================
+// ポリゴンの角度を設定する処理
+//=============================================================================
+void CTitleGear2D::SetAngle(const float fAngle)
+{
+	m_fAngle = fAngle;
+}
+
+//=============================================================================
+// ポリゴンの長さを設定する処理
+//=============================================================================
+void CTitleGear2D::SetLength(const float fLength)
+{
+	m_fLength = fLength;
+}
+
+//=============================================================================
+// 現在の向きを取得する処理
+//=============================================================================
+float CTitleGear2D::GetRot(void)
+{
+	return m_fRot;
+}
+
+//=============================================================================
+// 回転スピードを取得する処理
+//=============================================================================
+float CTitleGear2D::GetRotSpeed(void)
+{
+	return m_fRotSpeed;
+}
+
+//=============================================================================
+// ポリゴンの角度を取得する処理
+//=============================================================================
+float CTitleGear2D::GetAngle(void)
+{
+	return m_fAngle;
+}
+
+//=============================================================================
+// ポリゴンの長さを取得する処理
+//=============================================================================
+float CTitleGear2D::GetLength(void)
+{
+	return m_fLength;
+}
