2b4235cf6461e5e8b035a6fa69b058a44c058e1e
山下　敦史 <yamashitaatsushi19980502@gmail.com>
2019-11-12 13:17:35 +0900

    11月12日(火)結合

---
+++ 04_α版 (11月15日〆)/11月12(火)結合/powerplayer.cpp
@@ -0,0 +1,146 @@
+//=============================================================================
+//
+// モデル処理 [model.cpp]
+// Author : Kobayashi & Yamashita
+//
+//=============================================================================
+#include "player.h"
+#include "manager.h"
+#include "camera.h"
+#include "input.h"
+#include "inputmouce.h"
+#include "loadEffect.h"
+//*****************************************************************************
+// コンストラクタ
+//*****************************************************************************
+CPowerPlayer::CPowerPlayer()
+{
+
+}
+
+//*****************************************************************************
+// デストラクタ
+//*****************************************************************************
+CPowerPlayer::~CPowerPlayer()
+{
+
+}
+
+
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT CPowerPlayer::Init(D3DXVECTOR3 pos, char ModelTxt[40], char MotionTxt[40], int nNumPlayer)
+{
+	//プレイヤーのナンバーを格納
+	m_nNumPlayer = nNumPlayer;
+
+	//初期化
+	m_bWalk = true;
+
+	//キャラクターの初期化
+	CCharacter::Init(nNumPlayer,ModelTxt, MotionTxt);
+	CCharacter::SetPos(pos);
+
+	//モデルの取得
+	m_pModel = CCharacter::GetModel();
+
+	//ライン変数の初期化
+	m_nMaxLineTime = FIRST_LINETIME;
+	m_nLineTime = m_nMaxLineTime;
+	m_bBlockStartTerritory = false;
+	m_pOldStartTerritory = NULL;
+	m_nCountTime = 0;
+	m_pOrbitLine = NULL;
+	m_bMakeShape = false;
+	m_nCntTimeCopyLine = 0;
+
+	//コピーラインの初期化
+	for (int nCnt = 0; nCnt < MAX_TERRITORY; nCnt++)
+	{
+		m_apCopyLine[nCnt] = NULL;
+	}
+	//エフェクトの初期化
+	if (m_pLoadEffect == NULL)
+	{
+		m_pLoadEffect = CLoadEffect::Create(4, D3DXVECTOR3(pos.x, pos.y + 60.0f, pos.z), 5);
+	}
+	//モーションの情報を取得
+	m_pMotion = CCharacter::GetMotion();
+	m_fSpeed = 1.0f;
+	ResetLine();	//ラインの初期化
+
+	return S_OK;
+}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT CPowerPlayer::Init(void)
+{
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void  CPowerPlayer::Uninit(void)
+{
+	CPlayer::Uninit();
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void  CPowerPlayer::Update(void)
+{
+	CPlayer::Update();
+	//プレイヤーの移動
+	//SPlayerMove();
+}
+
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void  CPowerPlayer::Draw(void)
+{
+	CPlayer::Draw();
+}
+
+//=============================================================================
+//オブジェクトの生成処理
+//=============================================================================
+CPowerPlayer *CPowerPlayer::Create(D3DXVECTOR3 pos, char ModelTxt[40], char MotionTxt[40], int nNumPlayer, int nType, PLAYERTYPE playertype)
+{
+	CPowerPlayer *pPowerplayer = NULL;
+	if (pPowerplayer == NULL)
+	{
+		//メモリを動的確保
+		pPowerplayer = new CPowerPlayer;
+		if (pPowerplayer != NULL)
+		{//NULLチェック
+			pPowerplayer->SetType(nType);
+			pPowerplayer->m_PlayerType = playertype;
+			pPowerplayer->Init(pos, ModelTxt, MotionTxt, nNumPlayer);
+		}
+		else
+		{
+			MessageBox(0, "プレイヤーがNULLでした", "警告", MB_OK);
+		}
+	}
+	else
+	{
+		MessageBox(0, "プレイヤーがNULLじゃないです", "警告", MB_OK);
+	}
+	return pPowerplayer;
+}
+
+//=============================================================================
+//
+//=============================================================================
+void  CPowerPlayer::Set(const D3DXVECTOR3 pos, const D3DXVECTOR3 size)
+{
+
+}
