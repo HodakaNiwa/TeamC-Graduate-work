56f1c1d2b78c116092ac5fdd5adbcf6af3db592d
山下　敦史 <yamashitaatsushi19980502@gmail.com>
2019-12-02 09:37:45 +0900

    最新版

---
+++ 05_プレβ(12月13日〆)/mine.h
@@ -0,0 +1,102 @@
+//*****************************************************************************
+//
+//     地雷の処理[mine.h]
+//     Author:Hodaka Niwa
+//
+//*****************************************************************************
+#ifndef _MINE_H_
+#define _MINE_H_
+
+//*****************************************************************************
+//    インクルードファイル
+//*****************************************************************************
+#include "main.h"
+#include "sceneX.h"
+
+//*****************************************************************************
+//    マクロ定義
+//*****************************************************************************
+
+
+//*****************************************************************************
+//    前方宣言
+//*****************************************************************************
+class CLoadEffect;
+
+//*****************************************************************************
+//    地雷クラスの定義
+//*****************************************************************************
+class CMine : public CSceneX
+{
+public:    // 誰でもアクセス可能
+	//--------------------
+	//	状態
+	//--------------------
+	typedef enum
+	{
+		TYPE_NONE = 0,	// 通常状態
+		TYPE_STARTUP,	// 起動状態
+		TYPE_EXPLOSION,	// 爆発状態
+		TYPE_MAX
+	}TYPE;
+
+	CMine(int nPriority = 3, OBJTYPE objtype = OBJTYPE_MINE);
+	~CMine();
+
+	static CMine *Create(D3DXVECTOR3 pos, CScene *pParentChara);
+	static HRESULT Load(void);
+	static void UnLoad(void);
+
+	HRESULT Init(void);
+	void Uninit(void);
+	void Update(void);
+	void Draw(void);
+
+	//--------------------
+	//	Set & Get
+	//--------------------
+	void SetLife(const int nLife) { m_nLife = nLife; }
+	void SetType(const int nType) { m_nType = nType; }
+	void SetScaleUp(const float fScaleUp) { m_fScaleUp = fScaleUp; }
+	void SetExplosionTimer(const int nExplosionTimer) { m_nExplosionTimer = nExplosionTimer; }
+	void SetParentChara(CScene *pParentChara) { m_pParentChara = pParentChara; }
+
+	int GetType(void) { return m_nType; }
+	int GetLife(void) { return m_nLife; }
+	float GetScaleUp(void) { return m_fScaleUp; }
+	int GetExplosionTimer(void) { return m_nExplosionTimer; }
+	CScene *GetParentChara(void) { return m_pParentChara; }
+
+protected: // このクラスと派生クラスだけがアクセス可能
+
+private:   // このクラスだけがアクセス可能
+	//--------------------
+	//	関数
+	//--------------------
+	void None(void);
+	void StartUp(void);
+	void Explosion(void);
+	void CreateStartUpCollider(void);
+	void CreateExplosionEffect(void);
+	void CreateExplosionCollider(void);
+
+	//--------------------
+	//	メンバ変数
+	//--------------------
+	int m_nType;			// 状態
+	int m_nLife;			// 寿命
+	float m_fScaleUp;		// どれくらい大きくするか
+	int m_nExplosionTimer;	// 爆発までの時間を計測する
+	CScene *m_pParentChara; // 自身を生成したキャラへのポインタ
+	CLoadEffect * m_pLoadEffect;			//エフェクトの情報
+
+
+	//--------------------
+	//	静的メンバ変数
+	//--------------------
+	static LPD3DXMESH m_pMesh;
+	static LPD3DXBUFFER m_pBuffMat;
+	static DWORD m_nNumMat;
+};
+
+#endif
---
+++ 05_プレβ(12月13日〆)/tutorial.cpp
@@ -0,0 +1,126 @@
+//=============================================================================
+//
+// チュートリアルの処理 [tutorial.cpp]
+// Author : 佐藤 安純
+//
+//=============================================================================
+#include "tutorial.h"
+#include "main.h"
+#include "renderer.h"
+#include "manager.h"
+#include "input.h"
+#include "fade.h"
+#include "sound.h"
+#include "loadText.h"
+#include "camera.h"
+#include "fieldmanager.h"
+#include "loadText.h"
+#include "territory.h"
+#include "movie.h"
+#include "RawMouse.h"
+
+//*****************************************************************************
+// マクロ定義
+//*****************************************************************************
+#define LOAD_OBJ			("data/TEXT/Stage.txt")
+#define LOAD_TERRITORY		("data/TEXT/Territory.txt")
+#define MOVIE_NAME			(L"data/MOVIE/Tutorial.avi")
+
+//*****************************************************************************
+// 静的メンバ変数宣言
+//*****************************************************************************
+
+//=============================================================================
+// デフォルトコンストラクタ
+//=============================================================================
+CTutorial::CTutorial(){}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CTutorial::~CTutorial(){}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+void CTutorial::Init(void)
+{
+	//ベースの初期化
+	CModeBase::Init();
+
+	//変数の初期化
+	m_pMovie = NULL;
+
+	//動画の生成
+	if (m_pMovie == NULL)
+	{
+		m_pMovie = CMovie::Create(MOVIE_NAME);
+	}
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CTutorial::Uninit(void)
+{
+	//ベースの破棄
+	CModeBase::Uninit();
+
+	if (m_pMovie != NULL)
+	{
+		m_pMovie->Uninit();
+		delete m_pMovie;
+		m_pMovie = NULL;
+	}
+
+	//フェード以外削除
+	CScene::ReleaseFade();
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CTutorial::Update(void)
+{
+	//インプットの取得　
+	CGamePad	*pGamePad = CManager::GetInputGamePad();
+	CInputKeyboard * pKeyboard = CManager::GetInputkeyboard();
+	CInputXPad * pXPad = CManager::GetXPad();
+	CRawMouse *pRawMouse = CManager::GetRawMouse();					//RawMouseの取得
+	CInputKeyboard * pInputKeyboard = CManager::GetInputkeyboard();	//キーボードの取得
+
+	for (int nCnt = 0; nCnt < 4; nCnt++)
+	{
+		if (pKeyboard->GetKeyboardTrigger(DIK_RETURN) == true || pXPad->GetTrigger(XINPUT_GAMEPAD_A, nCnt) == true ||
+			pRawMouse->GetTrigger(CRawMouse::RIMS_BUTTON_LEFT, nCnt) == true)
+		{
+			if (CFade::GetFadeMode() != CFade::FADE_OUT) { CFade::SetFade(CManager::MODE_TITLE); }
+		}
+	}
+
+	//動画の更新処理
+	if (m_pMovie != NULL)
+	{
+		m_pMovie->Update();
+		bool bEndMovie = m_pMovie->GetEndMovie();
+
+		if (bEndMovie)
+		{
+			//動画の破棄
+			m_pMovie->Uninit();
+			delete m_pMovie;
+			m_pMovie = NULL;
+
+			//タイトルへ
+			if (CFade::GetFadeMode() != CFade::FADE_OUT) { CFade::SetFade(CManager::MODE_TITLE); }
+		}
+	}
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CTutorial::Draw(void)
+{
+	CScene::DrawAll();
+}
