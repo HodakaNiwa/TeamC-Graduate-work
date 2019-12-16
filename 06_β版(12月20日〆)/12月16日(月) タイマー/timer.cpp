1d1b8b37a472d11aec7001e13b08f8cdd4ed2b97
17_佐藤安純 <ctulbamsap@gmail.com>
2019-12-16 11:57:31 +0900

    タイマーの表示の仕方を変更

---
+++ 06_β版(12月20日〆)/12月16日(月) タイマー/timer.cpp
@@ -0,0 +1,597 @@
+//=============================================================================
+//
+// タイマーの処理 [timer.cpp]
+// Author : 山下敦史 & 佐藤安純
+//
+//=============================================================================
+#include "timer.h"
+#include "manager.h"
+#include "scene2D.h"
+#include "number2D.h"
+#include "scene.h"
+#include "fade.h"
+#include "timerlogo.h"
+#include "game.h"
+#include "fieldmanager.h"
+
+//=============================================================================
+// マクロ定義
+//=============================================================================
+#define MAX_TIME		(90) //180
+#define TIMERLOGO_SIZE (D3DXVECTOR3(200.0f, 80.0f, 0.0f))
+#define TIMERLOGO_SIZE2 (D3DXVECTOR3(125.0f, 50.0f, 0.0f))
+
+//=============================================================================
+// 静的メンバ変数宣言
+//=============================================================================
+//タイマークラス
+int CTimer::m_nTimeData[MAX_TIMENUMBER] = {};
+int CTimer::m_nTime = 0;
+
+//=============================================================================
+// 生成処理
+//=============================================================================
+CTimer * CTimer::Create(TYPE type, int Priority, D3DXVECTOR3 pos, D3DXVECTOR3 size)
+{
+	CTimer * pTimer = NULL;
+
+	if (type == TYPE_GAME)
+	{
+		if (pTimer == NULL)
+		{
+			pTimer = new CGameTimer(Priority, OBJTYPE_2DPOLYGON);
+
+			if (pTimer != NULL)
+			{
+				pTimer->SetSizePos(pos, size);
+				pTimer->Init();
+			}
+		}
+	}
+	if (type == TYPE_COUNTER)
+	{
+		if (pTimer == NULL)
+		{
+			pTimer = new CCountDownTimer(Priority, OBJTYPE_2DPOLYGON);
+
+			if (pTimer != NULL)
+			{
+				pTimer->SetSizePos(pos, size);
+				pTimer->Init();
+			}
+		}
+	}
+	return pTimer;
+}
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CTimer::CTimer(int nPriority, OBJTYPE objType) : CScene(nPriority, objType) {}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CTimer::~CTimer() {}
+
+//=============================================================================
+// タイムの代入
+//=============================================================================
+void CTimer::AddTime(int nTime)
+{
+	//変数宣言
+	int nCntNum;			//スコアの桁数のカウンター
+	int nNum = 1;
+	int nNum2 = 1;
+
+	//スコアの加算
+	m_nTime = nTime;
+
+	//桁数の計算
+	for (nCntNum = 0; nCntNum < MAX_TIMENUMBER; nCntNum++)
+	{
+		nNum *= 10;
+	}
+	for (nCntNum = 0; nCntNum < MAX_TIMENUMBER - 1; nCntNum++)
+	{
+		nNum2 *= 10;
+	}
+
+	for (nCntNum = 0; nCntNum < MAX_TIMENUMBER; nCntNum++)
+	{
+		m_nTimeData[nCntNum] = (m_nTime % nNum) / nNum2;	//格納する数字を計算
+
+															//桁数を１桁減らす
+		nNum = nNum / 10;
+		nNum2 = nNum2 / 10;
+	}
+}
+
+//=============================================================================
+// タイムの取得
+//=============================================================================
+int CTimer::GetTime(void)
+{
+	return m_nTime;
+}
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CCountDownTimer::CCountDownTimer(int nPriority, OBJTYPE objType) : CTimer(nPriority, objType) {}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CCountDownTimer::~CCountDownTimer() {}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT CCountDownTimer::Init()
+{
+	//変数宣言
+	int nCntNum;
+	int nCntNumber;
+
+	//数字のポインタの初期化
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		m_pNumber[nCntNumber] = NULL;
+	}
+
+	//数字の初期化
+	for (nCntNum = 0; nCntNum < MAX_TIMENUMBER; nCntNum++)
+	{
+		m_nNumber[nCntNum] = 0;
+	}
+
+	//初期値
+	m_nCountDown = 4;
+	m_nCounter = 0;
+	m_nDeathCount = 0;
+	m_bDeath = false;
+	m_bCountStop = false;
+	AddTime(3);
+	Set(m_pos, m_size);
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CCountDownTimer::Uninit(void)
+{
+	//変数宣言
+	int nCntNumber;
+
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		if (m_pNumber[nCntNumber] != NULL)
+		{
+			m_pNumber[nCntNumber]->Uninit();
+			delete m_pNumber[nCntNumber];
+			m_pNumber[nCntNumber] = NULL;
+		}
+	}
+
+	m_nTime = 0;
+
+	for (int nCntNum = 0; nCntNum < MAX_TIMENUMBER; nCntNum++)
+	{
+		m_nTimeData[nCntNum] = 0;
+	}
+
+	Release();
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CCountDownTimer::Update(void)
+{
+	//変数宣言
+	int nCntNumber;
+
+	CGame *pGame = CManager::GetGame();				// ゲームの取得←追加(よしろう)
+	CEventCamera *pEveCam = pGame->GetEveCam();		// イベントカメラの取得←追加(よしろう)
+	if (pEveCam == NULL)	// イベントカメラが消えていたら←追加(よしろう)
+	{
+		//カウントダウンの更新
+		TimeCountUpdate();
+	}
+
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		if (m_pNumber[nCntNumber] != NULL)
+		{
+			m_pNumber[nCntNumber]->Update();
+			m_pNumber[nCntNumber]->SetNumber(m_nTimeData[nCntNumber]);
+		}
+	}
+}
+
+//=============================================================================
+// カウントダウンの更新処理
+//=============================================================================
+void CCountDownTimer::TimeCountUpdate(void)
+{//安
+	//サウンドの取得
+	CSound *pSound = CManager::GetSound();
+
+	int nState = CGame::GetGameState();
+
+	m_nCounter++;
+
+	if (m_nCounter % 60 == 0)
+	{
+		m_nCountDown--;
+		CTimer::AddTime(m_nCountDown);
+
+		//カウントダウン時の音（開始時）
+		pSound->PlaySound(CSound::SOUND_LABEL_SE010);
+
+		if (m_nCountDown == 0)
+		{
+			//カウントダウン時の音（開始時）
+			pSound->StopSound(CSound::SOUND_LABEL_SE010);
+			//初期化する
+			m_nCountDown = 0;
+			m_bDeath = true;
+
+			//ゲーム状態にする
+			CGame::SetGameState(CGame::GAMESTATE_NORMAL);
+		}
+	}
+
+
+	if (m_bDeath == true)
+	{
+		CTimer::AddTime(1);
+
+		m_nDeathCount++;
+
+		if (m_nDeathCount == 5)
+		{
+			//スタート開始音
+			pSound->PlaySound(CSound::SOUND_LABEL_SE011);
+			//初期化する
+			m_nDeathCount = 0;
+			m_bDeath = false;
+
+			//タイマーの終了処理
+			Uninit();
+
+			//スタートロゴの生成 
+			CTimerLogo::Create(m_pos, TIMERLOGO_SIZE2, CTimerLogo::TIMERLOGO_START);
+		}
+	}
+	//音量の設定
+	pSound->SetVolume(CSound::SOUND_LABEL_SE010, 50.0f);
+	pSound->SetVolume(CSound::SOUND_LABEL_SE011, 30.0f);
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CCountDownTimer::Draw(void)
+{
+	//変数宣言
+	int nCntNumber;
+
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		if (m_pNumber[nCntNumber] != NULL)
+		{
+			m_pNumber[nCntNumber]->Draw();
+		}
+	}
+}
+
+//=============================================================================
+// 設定処理
+//=============================================================================
+void CCountDownTimer::Set(const D3DXVECTOR3 pos, const D3DXVECTOR3 size)
+{
+	//変数宣言
+	int nCntNumber;
+
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		if (m_pNumber[nCntNumber] == NULL)
+		{
+			m_pNumber[nCntNumber] = CNumber2D::Create(D3DXVECTOR3(pos.x + (nCntNumber * (size.x * 2)), pos.y, pos.z), size, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+
+			if (m_pNumber[nCntNumber] != NULL)
+			{
+				m_pNumber[nCntNumber]->SetbUse(true);
+			}
+		}
+	}
+}
+
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CGameTimer::CGameTimer(int nPriority, OBJTYPE objType) : CTimer(nPriority, objType) {}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CGameTimer::~CGameTimer() {}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT CGameTimer::Init()
+{
+	//変数宣言
+	int nCntNum;
+	int nCntNumber;
+
+	//数字のポインタの初期化
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		m_pNumber[nCntNumber] = NULL;
+	}
+
+	//数字の初期化
+	for (nCntNum = 0; nCntNum < MAX_TIMENUMBER; nCntNum++)
+	{
+		m_nNumber[nCntNum] = 0;
+	}
+
+	//初期値
+	m_nTimeCounter = 0;
+	m_nTime = MAX_TIME;
+	m_bTimeStop = false;
+	m_nDeathGameCount = 0;
+	m_FirstSize = INITIALIZE_VECTOR3;	//安
+	m_bDeth = false;	//安
+	Set(m_pos, m_size);
+	m_LogoSize = INITIALIZE_VECTOR3;	//ロゴのサイズ　安
+
+	//ロゴのサイズを設定する　安
+	int nNumPlayer = CManager::GetGame()->GetNumPlay();
+
+	if (nNumPlayer < 2)
+	{
+		m_LogoSize = TIMERLOGO_SIZE;
+	}
+	else
+	{
+		m_LogoSize = TIMERLOGO_SIZE2;
+	}
+
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CGameTimer::Uninit(void)
+{
+	//変数宣言
+	int nCntNumber;
+
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		if (m_pNumber[nCntNumber] != NULL)
+		{
+			m_pNumber[nCntNumber]->Uninit();
+
+			delete m_pNumber[nCntNumber];
+			m_pNumber[nCntNumber] = NULL;
+		}
+	}
+
+	m_nTime = 0;
+
+	for (int nCntNum = 0; nCntNum < MAX_TIMENUMBER; nCntNum++)
+	{
+		m_nTimeData[nCntNum] = 0;
+	}
+
+	Release();
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CGameTimer::Update(void)
+{
+	//変数宣言
+	int nCntNumber;
+
+	//ゲームタイムの更新
+	TimeGameUpdate();
+
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		if (m_pNumber[nCntNumber] != NULL)
+		{
+			m_pNumber[nCntNumber]->Update();
+			m_pNumber[nCntNumber]->SetNumber(m_nTimeData[nCntNumber]);
+		}
+	}
+}
+
+//=============================================================================
+// カウントダウンの更新処理
+//=============================================================================
+void CGameTimer::TimeGameUpdate(void)
+{//安
+	//サウンドの取得
+	CSound *pSound = CManager::GetSound();
+	int nState = CGame::GetGameState();
+
+	if (m_bTimeStop == true)
+	{
+		//ゲームを終了状態にする
+		CGame::SetGameState(CGame::GAMESTATE_END);
+
+		CTimer::AddTime(1);
+
+		m_nDeathGameCount++;
+
+		if (m_nDeathGameCount == 5)
+		{
+			//ゲーム終了音
+			pSound->PlaySound(CSound::CSound::SOUND_LABEL_SE014);
+
+			//フィニッシュロゴの生成
+			CTimerLogo::Create(m_pos, m_LogoSize, CTimerLogo::TIMERLOGO_FIN);
+
+			m_pNumber[0]->SetColor(INITIALIZE_VECTOR4);
+		}
+		if (m_nDeathGameCount >= 190)
+		{
+			//初期化する
+			m_nDeathGameCount = 0;
+			m_bTimeStop = false;
+
+			//タイマーが0になったらリザルトにフェードする
+			CFade::SetFade(CManager::MODE_RESULT);
+
+			//タイマーの終了処理
+			m_bDeth = true;
+		}
+	}
+
+	if (nState == CGame::GAMESTATE_NORMAL)
+	{//ゲーム状態の時
+		m_nTimeCounter--;
+
+		if (m_nTimeCounter % 60 == 0)
+		{
+			m_nTime--;
+			CTimer::AddTime(m_nTime);
+
+			if (m_nTime == 120)
+			{//残り2分になったらテクスチャを生成
+				pSound->PlaySound(CSound::CSound::SOUND_LABEL_SE012);	//タイムアナウンス
+				CTimerLogo::Create(m_pos, m_LogoSize, CTimerLogo::TIMERLOGO_2MIN);
+				CManager::GetGame()->GetFieldManger()->SetState(CFieldManager::STATE_TIME_2);
+			}
+			if (m_nTime == 115)
+			{//残り1分55秒になったらイベントを生成
+				StartUpEvent();
+			}
+			if (m_nTime == 60)
+			{//残り1分になったらテクスチャを生成
+				pSound->PlaySound(CSound::CSound::SOUND_LABEL_SE012);	//タイムアナウンス
+				CTimerLogo::Create(m_pos, m_LogoSize, CTimerLogo::TIMERLOGO_1MIN);
+				CManager::GetGame()->GetFieldManger()->SetState(CFieldManager::STATE_TIME_1);
+			}
+			if (m_nTime == 55)
+			{//残り55秒になったらテクスチャを生成
+				AppearRobot();
+			}
+			if (m_nTime == 30)
+			{ //残り30秒になったらテクスチャを生成
+				pSound->PlaySound(CSound::CSound::SOUND_LABEL_SE012);	//タイムアナウンス
+				CTimerLogo::Create(m_pos, m_LogoSize, CTimerLogo::TIMERLOGO_30SEC);
+				CManager::GetGame()->GetFieldManger()->SetState(CFieldManager::STATE_TIME_30);
+			}
+			if (m_nTime <= 5 && m_nTime >= 1)
+			{//残り5秒でSEを鳴らす
+				pSound->PlaySound(CSound::CSound::SOUND_LABEL_SE013);	//カウントダウン(終了時)
+
+				//安
+				for (int nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+				{//タイマーの色を変える
+					if (m_pNumber[nCntNumber] != NULL)
+					{
+						m_pNumber[nCntNumber]->SetColor(D3DXCOLOR(1.0f, 0.0f, 0.0f, 1.0f));
+					}
+				}
+			}
+			if (m_nTime == 0)
+			{
+				//値を初期化する
+				m_bTimeStop = true;
+				m_nTime = 0;
+			}
+		}
+	}
+
+	if (m_bDeth == true) { Uninit(); }
+}
+
+//=============================================================================
+// イベントを起動する処理
+//=============================================================================
+void CGameTimer::StartUpEvent(void)
+{
+	// ゲームクラスの取得
+	CGame *pGame = CManager::GetGame();
+	if (pGame == NULL) { return; }
+
+	// 起動させるイベントをランダムで設定
+	int nRandom = rand() % CGame::GAMEEVENT_MAX;
+
+	switch (nRandom)
+	{// ランダムの値によって処理わけ
+	case CGame::GAMEEVENT_AREABONUS:
+		pGame->StartAreaBonusEvent();
+		break;
+	case CGame::GAMEEVENT_DIVISION:
+		pGame->StartDivisionEvent();
+		break;
+	}
+}
+
+//=============================================================================
+// ロボットを出現させる処理
+//=============================================================================
+void CGameTimer::AppearRobot(void)
+{
+	// ゲームクラスの取得
+	CGame *pGame = CManager::GetGame();
+	if (pGame == NULL) { return; }
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CGameTimer::Draw(void)
+{
+	//変数宣言
+	int nCntNumber;
+
+	if (m_nTime <= 5)
+	{//ゲームタイマーが5秒になったら描画する
+		for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+		{
+			if (m_pNumber[nCntNumber] != NULL)
+			{
+				m_pNumber[nCntNumber]->Draw();
+			}
+		}
+	}
+}
+
+//=============================================================================
+// 設定処理
+//=============================================================================
+void CGameTimer::Set(const D3DXVECTOR3 pos, const D3DXVECTOR3 size)
+{//安
+	//変数宣言
+	int nCntNumber;
+
+	for (nCntNumber = 0; nCntNumber < MAX_TIMENUMBER; nCntNumber++)
+	{
+		if (m_pNumber[nCntNumber] == NULL)
+		{
+			m_FirstSize = size /** 4.0f*/;
+			m_pNumber[nCntNumber] = CNumber2D::Create(D3DXVECTOR3(pos.x + (nCntNumber * (m_FirstSize.x * 2)), pos.y, pos.z), m_FirstSize, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+
+			if (m_pNumber[nCntNumber] != NULL)
+			{
+				m_pNumber[nCntNumber]->SetbUse(true);
+			}
+		}
+	}
+}
---
+++ 06_β版(12月20日〆)/12月16日(月) タイマー/timerlogo.cpp
@@ -0,0 +1,212 @@
+//=============================================================================
+//
+// タイマーロゴの処理 [timerlogo.cpp]
+// Author : 山下敦史 & 佐藤安純
+//
+//=============================================================================
+#include "timerlogo.h"
+#include "manager.h"
+#include "input.h"
+#include "renderer.h"
+#include "scene.h"
+#include "input.h"
+#include "logo.h"
+#include "bottun.h"
+
+//=============================================================================
+// 静的メンバ変数宣言
+//=============================================================================
+LPDIRECT3DTEXTURE9 CTimerLogo::m_pTexture[TIMERLOGO_MAX] = {};
+
+//=============================================================================
+// マクロ定義
+//=============================================================================
+#define VECTOR_ZERO		(D3DXVECTOR3(0.0f, 0.0f, 0.0f))	//ベクトルの初期化
+
+//=============================================================================
+// 生成処理
+//=============================================================================
+CTimerLogo * CTimerLogo::Create(const D3DXVECTOR3 pos,const D3DXVECTOR3 size,TIMERLOGO timerlogo)
+{
+	//インスタンスの生成
+	CTimerLogo * pTimerLogo = NULL;
+
+	if (pTimerLogo == NULL)
+	{
+		pTimerLogo = new CTimerLogo;
+
+		if (pTimerLogo != NULL)
+		{
+			pTimerLogo->m_Size = size;
+			pTimerLogo->m_FirstSize = size * 2.0f;	//安
+			pTimerLogo->m_pos = pos;
+			pTimerLogo->m_TimerLogo = timerlogo;
+			pTimerLogo->BindTexture(m_pTexture[timerlogo]);
+			pTimerLogo->Init();
+			pTimerLogo->SetColoer(INITIALIZE_VECTOR4);
+		}
+	}
+	return pTimerLogo;
+}
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CTimerLogo::CTimerLogo(int nPriority, CScene::OBJTYPE type)
+{
+	m_pVtxBuff = NULL;		// 頂点バッファへのポインタ
+	m_pos = VECTOR_ZERO;	// ポリゴンの位置
+	m_Size = VECTOR_ZERO;	// ポリゴンのサイズ
+	m_FirstSize = VECTOR_ZERO; //最初のサイズ　安
+	m_bDeth = false;			//死亡フラグ　安
+}
+
+//=============================================================================
+// コンストラクタ
+//=============================================================================
+CTimerLogo::CTimerLogo()
+{
+	m_pVtxBuff = NULL;		// 頂点バッファへのポインタ
+	m_pos = VECTOR_ZERO;	// ポリゴンの位置
+	m_Size = VECTOR_ZERO;	// ポリゴンのサイズ
+	m_FirstSize = VECTOR_ZERO; //最初のサイズ　安
+	m_bDeth = false;			//死亡フラグ　安
+}
+
+//=============================================================================
+// デストラクタ
+//=============================================================================
+CTimerLogo::~CTimerLogo() {}
+
+//=============================================================================
+// テクスチャの読み込み処理
+//=============================================================================
+HRESULT CTimerLogo::Load(void)
+{
+	LPDIRECT3DDEVICE9 pDevice;
+
+	//インスタンス（マネージャー）
+	CManager Manager;
+
+	//デバイスを取得
+	pDevice = Manager.GetRenderer()->GetDevice();
+
+	// テクスチャの生成
+	D3DXCreateTextureFromFile(pDevice, "data\\TEXTURE\\game\\game_start000.png", &m_pTexture[0]);			//スタートの画像
+	D3DXCreateTextureFromFile(pDevice, "data\\TEXTURE\\game\\time_announce000.png", &m_pTexture[1]);		//2分の画像
+	D3DXCreateTextureFromFile(pDevice, "data\\TEXTURE\\game\\time_announce001.png", &m_pTexture[2]);		//1分の画像
+	D3DXCreateTextureFromFile(pDevice, "data\\TEXTURE\\game\\time_announce002.png", &m_pTexture[3]);		//30秒の画像
+	D3DXCreateTextureFromFile(pDevice, "data\\TEXTURE\\game\\game_finish000.png", &m_pTexture[4]);			//フィニッシュの画像
+
+	return S_OK;
+}
+//=============================================================================
+// テクスチャの開放処理
+//=============================================================================
+void CTimerLogo::Unload(void)
+{
+	for (int nCnt = 0; nCnt < TIMERLOGO_MAX; nCnt++)
+	{
+		//テクスチャの破棄
+		if (m_pTexture[nCnt] != NULL)
+		{
+			m_pTexture[nCnt]->Release();
+			m_pTexture[nCnt] = NULL;
+		}
+	}
+}
+
+//=============================================================================
+// 初期化処理
+//=============================================================================
+HRESULT CTimerLogo::Init(void)
+{
+	CScene2D::Init();
+
+	//初期値
+	m_Color = D3DXCOLOR(1.0f, 1.0f,1.0f,1.0f);
+	SetColoer(m_Color);
+
+	if (m_TimerLogo == TIMERLOGO_2MIN || m_TimerLogo == TIMERLOGO_1MIN || m_TimerLogo == TIMERLOGO_30SEC)
+	{
+		Set(m_pos, m_FirstSize);
+	}
+	else
+	{
+		Set(m_pos, m_Size);
+	}
+
+	return S_OK;
+}
+
+//=============================================================================
+// 終了処理
+//=============================================================================
+void CTimerLogo::Uninit()
+{
+	CScene2D::Uninit();
+}
+
+//=============================================================================
+// 更新処理
+//=============================================================================
+void CTimerLogo::Update()
+{//安
+	if (m_TimerLogo == TIMERLOGO_2MIN || m_TimerLogo == TIMERLOGO_1MIN || m_TimerLogo == TIMERLOGO_30SEC)
+	{//FINテクスチャじゃなかったら
+
+		//変数宣言
+		bool m_SetSizeX = false;
+		bool m_SetSizeY = false;
+
+		//縮小する
+		m_FirstSize.x -= 2.8f;
+		m_FirstSize.y -= 2.5f;
+
+		//値の修正
+		if (m_FirstSize.x < m_Size.x)
+		{
+			m_FirstSize.x = m_Size.x;
+			m_SetSizeX = true;
+		}
+		if (m_FirstSize.y < m_Size.y)
+		{
+			m_FirstSize.y = m_Size.y;
+			m_SetSizeY = true;
+		}
+
+		//値を設定する
+		Set(m_pos, m_FirstSize);
+
+		if (m_SetSizeX && m_SetSizeY)
+		{
+			m_Color.a -= 0.01f;
+
+			if (m_Color.a < 0.0f) { m_bDeth = true; }
+		}
+
+		SetColoer(D3DXCOLOR(1.0f, 1.0f, 1.0f, m_Color.a));
+	}
+	else if (m_TimerLogo == TIMERLOGO_START)
+	{//色の設定
+		
+		m_Color.a -= 0.01f;
+		SetColoer(D3DXCOLOR(1.0f, 1.0f, 1.0f, m_Color.a));
+		if (m_Color.a < 0.0f) { m_bDeth = true; }
+	}
+	else if(m_TimerLogo == TIMERLOGO_FIN)
+	{//色の設定
+		SetColoer(D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+	}
+
+	//死亡フラグが有効だったら
+	if (m_bDeth) { Uninit(); }
+}
+
+//=============================================================================
+// 描画処理
+//=============================================================================
+void CTimerLogo::Draw()
+{
+	CScene2D::Draw();
+}
